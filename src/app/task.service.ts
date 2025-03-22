e267adb95933248cfc2dc53b8fb9ead55983b4fa
Bigghis <pa.casciano@gmail.com>
2025-03-22 18:16:53 +0100

    handle random types in service

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Task } from './model/task';
+import { TaskType } from './model/taskType';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
@@ -20,6 +21,7 @@ export class TaskService {
   getTasks(): Observable<Task[]>  {
    return this.http.get<Task[]>(TASKS_URL).pipe(
     map(tasks => tasks.slice(0, TASKS_NUM)),
+    map(tasks => tasks.map(task => ({ ...task, type: this.getRandomTaskType() }))),
     tap(tasks => console.log('Tasks fetched:', tasks)),
     tap(tasks => this.tasks = tasks),
     catchError(error => {
@@ -38,10 +40,17 @@ export class TaskService {
     return this.http.put<Task>(`${TASKS_URL}/${id}`, { completed: true });
   }
 
+  private getRandomTaskType(): TaskType {
+    const taskTypes = Object.values(TaskType);
+    const randomIndex = Math.floor(Math.random() * taskTypes.length);
+    return taskTypes[randomIndex];
+  }
+
 
   addRandomTask(): Observable<Task> {
     return this.http.get<Task[]>(TASKS_URL).pipe(
       map(tasks => tasks[Math.floor(Math.random() * tasks.length)]),
+      map(task => ({ ...task, type: this.getRandomTaskType() })),
       tap(task => console.log('Random task fetched:', task)),
       catchError(error => {
         console.error('Error fetching random task:', error);
